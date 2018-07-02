cdb659a5a6f846afdd0ef18964fe9003fce486e0
Samuel Henry Kurniawan <samuelhenrykurniawan@yahoo.com>
2018-07-02 09:31:31 +0800

    add new project, caesar cipher

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,9 @@
 #write your code here
+def translate(input)
+  vocal = ["a", "i", "u", "e", "o"]
+  if (vocal.include? input[0])
+    return input + "ay"
+  else
+    return input.reverse + "ay"
+  end
+end
